4d14222837f2607ed41111aa67f1d69624e3f40b
Fikcup <rhys.n.wright@gmail.com>
2021-11-13 11:27:14 -0800

    feat : hello world code

---
+++ 00-hello-world/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _00_Check_Installation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
